0404fa63d0a255ad8e04f09cfb50bdf9c1ebf923
Floris Jolink <floris.jolink@gmail.com>
2018-03-13 10:47:35 +0100

    Added storage module

--- src/main/java/me/theblockbender/fantastic/treasure/Treasure.java
+++ src/main/java/me/theblockbender/fantastic/treasure/Treasure.java
@@ -5,10 +5,15 @@ import me.theblockbender.fantastic.treasure.manager.TreasureChest;
 import me.theblockbender.fantastic.treasure.util.SchematicHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Treasure extends JavaPlugin {
@@ -17,6 +22,7 @@ public class Treasure extends JavaPlugin {
     public SchematicHandler schematicHandler;
     public HashMap<Location, TreasureChest> treasureChests = new HashMap<>();
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
     public void onEnable() {
         getLogger().info("Enabling treasure chest feature.");
@@ -27,7 +33,9 @@ public class Treasure extends JavaPlugin {
         saveDefaultConfig();
         if (!getConfig().getString("version").equalsIgnoreCase(this.getDescription().getVersion())) {
             getLogger().warning("Your configuration file is outdated.");
+            getLogger().warning("This might lead to problems with the plugin.");
         }
+        loadTreasure();
         language = new Language(this);
         saveResource("resources" + File.separator + "haunted_treasure.schematic", true);
         saveResource("resources" + File.separator + "legendary_treasure.schematic", true);
@@ -39,6 +47,37 @@ public class Treasure extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Disabling treasure chest feature.");
+    }
+
+    private void loadTreasure() {
+        List<String> strings = getConfig().getStringList("chests");
+        if (strings == null || strings.isEmpty())
+            return;
+        for (String string : strings) {
+            Location chest;
+            String[] parts = string.split("\\|");
+            try {
+                chest = new Location(Bukkit.getWorld(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+                Block block = chest.getBlock();
+                if (block.getType() == Material.CHEST) {
+                    MaterialData data = block.getState().getData();
+                    treasureChests.put(chest, new TreasureChest(this, chest, data));
+                }
+            } catch (Exception ignored) {
+            }
+        }
+        strings.clear();
+        saveTreasure();
+    }
+
+    public void saveTreasure(){
+        List<String> strings = new ArrayList<>();
+        for (Map.Entry<Location, TreasureChest> entry : treasureChests.entrySet()) {
+            Location accepted = entry.getKey();
+            strings.add(accepted.getWorld().getName() + "|" + accepted.getBlockX() + "|" + accepted.getBlockY() + "|" + accepted.getBlockZ());
+        }
+        getConfig().set("chests", strings);
+        saveConfig();
     }
 
     private void startTasks() {
